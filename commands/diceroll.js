01351130a1c1a7462dc8f83ff822d448c96739de
Clam <dfunking@gmail.com>
2018-11-25 14:53:17 -0600

    Added dice

--- commands/diceroll.js
+++ commands/diceroll.js
@@ -1,15 +1,36 @@
 const d20 = require("d20");
 const {MessageEmbed} = require("discord.js");
+
+function median(values){
+  values.sort(function(a,b){
+  return a-b;
+});
+
+if(values.length ===0) return 0
+
+var half = Math.floor(values.length / 2);
+
+if (values.length % 2)
+  return values[half];
+else
+  return (values[half - 1] + values[half]) / 2.0;
+}
+
 exports.run = async (client, message, args) => {
-  const diceRolls = d20.verboseRoll(args[0] || "1d6");
+  let diceRolls = d20.verboseRoll(args[0] || "1d6");
+  diceRolls = diceRolls.sort((a, b) => a - b);
+  let med = diceRolls[(diceRolls.length + 1) /2];
   const embed = new MessageEmbed()
     .setTitle("DICE!")
     .setDescription(diceRolls.join(", "))
     .addField("Average", client.average(diceRolls), true)
     .addField("Sum", diceRolls.reduce((a, b) => a + b), true)
+    .addField("Median", median(diceRolls));
 
   message.channel.send(embed)
 };
+
+
 
 
 exports.help = {
